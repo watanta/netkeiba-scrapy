e51ffe222e78388f8254bd1e86da99082ccde784
takahiro.watanabe <takahiro.watanabe@yahoo.co,jp>
2020-12-29 10:28:57 +0900

    enable cache

--- netkeiba/netkeiba/settings.py
+++ netkeiba/netkeiba/settings.py
@@ -84,8 +84,10 @@ DOWNLOAD_DELAY = 1
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# horse_crawlerを動かすときは以下の3行をコメントアウト
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 365* 60 * 60 * 24
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
